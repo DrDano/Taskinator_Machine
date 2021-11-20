603ac2452d1f5f5ccdbfeac40f382ecb00985f94
DrDano <danielharned@gmail.com>
2021-11-19 16:03:11 -0800

    adding form input query to set list item text content

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,12 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function(e) {
     e.preventDefault();
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task";
+    listItemEl.textContent = taskNameInput;
     tasksToDoEl.appendChild(listItemEl);
-}
+};
 
 formEl.addEventListener("submit", createTaskHandler);
