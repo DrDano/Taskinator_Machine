4a46bb5d814850112a474974c5baaf2e7d76aa0e
DrDano <danielharned@gmail.com>
2021-11-20 18:54:30 -0800

    adding query selectors to taskEdit that select for the task name and type

--- assets/js/script.js
+++ assets/js/script.js
@@ -95,9 +95,14 @@ var deleteTask = function(taskId) {
 };
 
 var editTask = function(taskId) {
-    console.log("editing task #" + taskId);
+var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
 
-    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+// get content from task name and type
+var taskName = taskSelected.querySelector("h3.task-name").textContent;
+console.log(taskName);
+
+var taskType = taskSelected.querySelector("span.task-type").textContent;
+console.log(taskType);
 }
 
 var taskButtonHandler = function(event) {
