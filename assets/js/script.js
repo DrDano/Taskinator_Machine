50305eb6fdf62a868e83779ada0bc49b53bbdd73
DrDano <danielharned@gmail.com>
2021-11-21 17:15:14 -0800

    adding evaluator block within for loop to place loaded items into their appropriate task column in the GUI

--- assets/js/script.js
+++ assets/js/script.js
@@ -228,18 +228,42 @@ loadTasks = function(tasks) {
         tasks[i].id = taskIdCounter;
         console.log(tasks[i])
 
-        var listitemEl = document.createElement("li");
-        listitemEl.className = "task-item";
-        listitemEl.setAttribute("data-task-id", tasks[i].id)
+        var listItemEl = document.createElement("li");
+        listItemEl.className = "task-item";
+        listItemEl.setAttribute("data-task-id", tasks[i].id)
 
         var taskInfoEl = document.createElement("div")
         taskInfoEl.className = "task-info"
         taskInfoEl.innerHTML = "<h3 class='task-name'>" + tasks[i].name + "</h3><span class='task-type'>" + tasks[i].type + "</span>";
         
-        listitemEl.appendChild(listitemEl);
+        listItemEl.appendChild(taskInfoEl);
 
         var taskActionsEl = createTaskActions(tasks[i].id)
-        listitemEl.append(taskActionsEl)
+        listItemEl.append(taskActionsEl)
+
+        console.log(listItemEl);
+
+        if (tasks[i].status.toLowerCase === "to do") {
+            listItemEl.querySelector("select[name'status-change'").selectedIndex = 0
+
+            tasksToDoEl.appendChild(listItemEl)
+        }
+
+        else if (tasks[i].status.toLowerCase === "in-progress") {
+            listItemEl.querySelector("select[name='status-change'").selectedIndex = 1
+
+            tasksInProgressEl.appendChild(listItemEl)
+        }
+
+        else if (tasks[i].status.toLowerCase === "complete") {
+            listItemEl.querySelector("select[name='status-change']").selectedIndex = 2
+
+            tasksCompletedEl.appendChild(listItemEl)
+        }
+
+        taskIdCounter++
+
+        console.log(listItemEl)
     }
 }
 
